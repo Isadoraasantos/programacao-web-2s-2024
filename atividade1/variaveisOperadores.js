d89c2fa41cfa5fd94566d9778963603d3bcbe856
Isadoraasantos <isasdorasantos@gmail.com>
2024-08-16 10:53:20 -0300

    Adicionar

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,7 @@
+let n1  = 2;
+let n2 = 3;
+
+console.log(`O resultado da soma entre ${n1} e ${n2} é ${n1 + n2}`);
+console.log(`O resultado da subtração é ${n1} e ${n2} é ${n1 - n2}`);
+console.log(`O resultado da multiplicação é ${n1} e ${n2} é ${n1 * n2}`);
+console.log(`O resultado da divisão é ${n1} e ${n2} é ${n1 / n2}`);
