c543eeba34d4b11ad8462fe111fb317f8ad70521
Isadoraasantos <isasdorasantos@gmail.com>
2024-09-20 10:23:44 -0300

    adiconar

---
+++ atividade3/index.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+const calc = require('./util/calculadora');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('Atividade3 - APP Calculadora');
+});
+
+app.get('/somar/:a/:b', (req, res)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let c = calc.somar(a, b);
+    res.send(`${a} + ${b} = ${c}`);
+});
+
+app.get('/subtrair/:a/:b', (req, res)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let c = calc.subtrair(a, b);
+    res.send(`${a} - ${b} = ${c}`);
+});
+
+app.get('/multiplicar/:a/:b', (req, res)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let c = calc.multiplicar(a, b);
+    res.send(`${a} * ${b} = ${c}`);
+});
+
+app.get('/dividir/:a/:b', (req, res)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let c = calc.dividir(a, b);
+    res.send(`${a} / ${b} = ${c}`);
+});
+
+const PORT = 8080;
+app.listen(PORT, ()=>{
+    console.log('app executando na porta' + PORT);
+});
+
+
+
+
+
+
+
+
+
+
+
+
